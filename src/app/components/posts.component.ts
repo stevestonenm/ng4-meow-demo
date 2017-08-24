37e3c78b8399e32d9959378e8d95e127652e4a99
Steve Stone <stoneflynm@gmail.com>
2017-08-24 16:31:15 -0600

    updated posts component

--- src/app/components/posts.component.ts
+++ src/app/components/posts.component.ts
@@ -1,9 +1,21 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
+import {PostService} from "../services/post-service";
+import {Posts} from "../classes/posts";
 
 @Component({
 	templateUrl: "./templates/posts.php"
 })
 
-export class PostsComponent {}
+export class PostsComponent {
+
+	posts : Posts[]
+
+	constructor(protected postService: PostService) {}
+
+	getAllPosts() : void {
+		this.postService.getAllPosts()
+			.subscribe(posts=>this.posts=posts);
+	}
+}
