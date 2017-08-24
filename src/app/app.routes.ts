070678b1b2b70c0e1c16c6eb4f2d42c6751cabb3
Steve Stone <stoneflynm@gmail.com>
2017-08-24 11:04:10 -0600

    added post to angular template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {AboutComponent} from "./components/about.component";
+import {PostsComponent} from "./components/posts.component";
 
 export const allAppComponents = [
 	HomeComponent,
-	AboutComponent
+	AboutComponent,
+	PostsComponent
 ];
 
 export const routes: Routes = [
+	{path: "posts", component:PostsComponent},
 	{path: "about", component:AboutComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
